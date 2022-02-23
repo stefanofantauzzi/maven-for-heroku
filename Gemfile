c0986a5bdfff23c91966f2c7fd6852bb4ad2bdbf
Stefano <s.fantauzzi@accenture.com>
2022-02-24 00:09:16 +0100

    add requirements.txt runtime.txt

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+gem 'heroku_hatchet'
+gem 'rspec-retry'
+gem 'rspec-expectations'
+gem 'parallel_split_test'
+gem 'java-properties'
+gem 'docker-api'
---
+++ test/stdlib_stubs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+export BUILDPACK_LOG_FILE="$(mktemp)"
+
+mmeasure() {
+  :
+}
+
+mcount() {
+  :
+}
+
+mtime() {
+  :
+}
+
+mnow() {
+  date +%s%3N
+}
+
+nowms() {
+  date +%s%3N
+}
