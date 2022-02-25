384b7d7ed4394bcf9867c2c894a6fa7dd8c20b21
Stefano <s.fantauzzi@accenture.com>
2022-02-25 18:22:22 +0100

    add -Dspotless.check.skip

--- lib/maven.sh
+++ lib/maven.sh
@@ -19,8 +19,8 @@ _mvn_cmd_opts() {
   local scope=${1}
 
   if [ "$scope" = "compile" ]; then
-    echo -n "${MAVEN_CUSTOM_OPTS:-"-DskipTests"}"
-    echo -n " ${MAVEN_CUSTOM_GOALS:-" clean install -DskipTests=true -Dspotless.check.skip"}"
+    echo -n "${MAVEN_CUSTOM_OPTS:-"-DskipTests=true -Dspotless.check.skip=true -Dspotbugs.skip=true"}"
+    echo -n " ${MAVEN_CUSTOM_GOALS:-" clean install"}"
   elif [ "$scope" = "test-compile" ]; then
     echo -n "${MAVEN_CUSTOM_GOALS:-"clean dependency:resolve-plugins test-compile"}"
   else
